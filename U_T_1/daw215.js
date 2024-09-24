af1200d29a15ae118f04f71bd22e747d7a86f312
Henzel <hancelfernando12@gmail.com>
2024-09-24 16:21:30 +0200

    Modificado archivo daw215.js con una petición para una suma

--- U_T_1/daw215.js
+++ U_T_1/daw215.js
@@ -1,1 +1,6 @@
-console.log("Hola soy el usuario DAW215");
+console.log("Hola soy el usuario DAW215")
+let num1 = parseInt(prompt("Dame un numero entre 1 y 10", "Numerico por favor"))
+let num2 = parseInt(prompt("Dame el segundo numero para sumarlo", "Numerico por favor"))
+let resultado = num1 + num2
+
+alert("El resultado de la suma es " + resultado)
