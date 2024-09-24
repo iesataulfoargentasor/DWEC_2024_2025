2f427d4fd3a7e0d6865c29404a554c1425188832
IgrSlvDMrl <isilva.demeireles@gmail.com>
2024-09-24 16:11:31 +0200

    Añadadido suma de dos variables introducidas por el usuario

--- U_T_1/daw219.js
+++ U_T_1/daw219.js
@@ -1,1 +1,6 @@
 console.log("Hola soy el usuario DAW219");
+
+let a = parseInt(prompt("introduzca un numero para sumar: "));
+let b = parseInt(prompt("introduzca segundo numero para sumar: "));
+
+alert(`la suma de los dos numeros es ${(a+b)}`);
