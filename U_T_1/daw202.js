ed8f2f529515c248bbc01e752b7417b256e1a6e3
EduOlalde <edolaldecruz@gmail.com>
2024-09-24 15:27:46 +0200

    Modificado archivo js para incluir promt y suma de numeros

--- U_T_1/daw202.js
+++ U_T_1/daw202.js
@@ -1,1 +1,6 @@
 console.log("Hola soy el usuario DAW202");
+
+let num1 = parseInt(prompt("Dame un número entre 1 y 10", " Numérico, por favor"));
+let num2 = parseInt(prompt("Dame el segundo número para sumarlo", " Numérico, por favor"));
+
+alert(`El resultado de la suma de los números es: ${(num1 + num2)}`);
