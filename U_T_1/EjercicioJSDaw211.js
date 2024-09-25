7cfdb819f94f27c0c1cac0ad160bf18e54a254e6
Bruno <herrerabrunoo11@gmail.com>
2024-09-25 11:40:30 +0200

    ejercicio JS de daw211

---
+++ U_T_1/EjercicioJSDaw211.js
@@ -0,0 +1,8 @@
+let num1 = parseInt(prompt("dame un numero 1-10", "numerico pls"));
+let num2 = parseInt(prompt("dame el segundo 1-10", "numerico pe"));
+
+
+
+let resultado = num1 + num2; 
+console.log()
+alert(`el resultado es: ${(num1+num2)}`)
